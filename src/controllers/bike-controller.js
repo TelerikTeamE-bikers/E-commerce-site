7acf50b05c7985034c7666748a441ce9beedad9a
Savata <sava.ivanov.9@gmail.com>
2017-06-28 08:40:57 +0300

    testing mongo

--- src/controllers/bike-controller.js
+++ src/controllers/bike-controller.js
@@ -1,4 +1,27 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const mongoose = require('mongoose');
+const Unit = mongoose.model('Unit', { unitType: String });
+
 const loadAllBikes = (req, res) => {
+    //mongoose.connect('mongodb://localhost:27017/DatabaseName')
+    
+    // MongoClient.connect('mongodb://localhost:27017/DatabaseName')
+    //     .then((db)=> {
+    //         console.log(db);
+    //         // query the database...
+    //     })
+
+    var b = new Unit({ unitType: 'Bike' }).save(() =>{
+        var a = Unit.find({ unitType: 'Bike' }).exec(() => {
+            console.log(a);
+        });
+    }); // create
+
+    // var a = Unit.find({ unitType: 'Bike' }).exec(callback); // fetch
+
+    // console.log(a);
+
     res.render('Bikes', {
         title: 'Bike Data',
         header: 'All Bikes:',
