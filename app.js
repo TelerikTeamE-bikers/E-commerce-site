4907feafb2e1c8d33b4c96205186944d4823a1ce
Savata <sava.ivanov.9@gmail.com>
2017-07-02 15:16:16 +0300

    constants fix

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', './src/views');
 
+const constants = require('../common/constants');
 const mainRoutes = require('./src/routes');
 const allBikesRoutes = require('./src/routes/allBikes.js');
 const spareParts = require('./src/routes/spareParts.js');
@@ -40,10 +41,10 @@ app.use((err, req, res, next) => {
     res.render('error')
 })
 
-app.listen(3030, () => {
+app.listen(constants.APP_PORT, () => {
     const date = new Date();
     console.log('  >Log--' + date.toLocaleTimeString() +
-        ` :: App running at :3030`);
+        ` :: App running at :` + constants.APP_PORT);
 });
 
 module.exports.app = app;
--- src/controllers/bike-controller.js
+++ src/controllers/bike-controller.js
@@ -1,38 +1,38 @@
-/*const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
-const protocol = 'mongodb:/';
-const server = 'localhost:27017';
-const databaseName = 'testDb';
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const protocol = 'mongodb:/';
+// const server = 'localhost:27017';
+// const databaseName = 'testDb';
 
-const loadAllBikes = (req, res) => {
-    //mongoose.connect('mongodb://localhost:27017/DatabaseName')
+// const loadAllBikes = (req, res) => {
+//     //mongoose.connect('mongodb://localhost:27017/DatabaseName')
 
-    // MongoClient.connect('mongodb://localhost:27017/DatabaseName')
-    //     .then((db)=> {
-    //         console.log(db);
-    //         // query the database...
-    //     })
+//     // MongoClient.connect('mongodb://localhost:27017/DatabaseName')
+//     //     .then((db)=> {
+//     //         console.log(db);
+//     //         // query the database...
+//     //     })
 
-    // console.log(a);
+//     // console.log(a);
 
-    res.render('Bikes', {
-        title: 'Bike Data',
-        header: 'All Bikes:',
-        message: 'no bikes for now',
-    });
-};
+//     res.render('Bikes', {
+//         title: 'Bike Data',
+//         header: 'All Bikes:',
+//         message: 'no bikes for now',
+//     });
+// };
 
-// app.get("/:id", (req, res) => {
-//   res.send(superheroes[req.params.id]);
-// });
+// // app.get("/:id", (req, res) => {
+// //   res.send(superheroes[req.params.id]);
+// // });
 
-// app.post("/", (req, res) => {
-//   let superhero = req.body;
-//   superhero.id = superheroes.length;
-//   superheroes.push(superhero)
-//   res.send(superhero);
-// });
+// // app.post("/", (req, res) => {
+// //   let superhero = req.body;
+// //   superhero.id = superheroes.length;
+// //   superheroes.push(superhero)
+// //   res.send(superhero);
+// // });
 
-module.exports = {
-    loadAllBikes,
-};*/
+// module.exports = {
+//     loadAllBikes,
+// };
