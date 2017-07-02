ce82410bcd40722074c3f5ee47371ac11f392741
Savata <sava.ivanov.9@gmail.com>
2017-07-02 14:48:12 +0300

    added bike and user models

---
+++ src/common/constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+    APP_PORT: 3030,
+    DB_URL: 'mongodb://localhost:27017/testDb',
+    MIN_USERNAME_LENGTH: 5,
+    MAX_USERNAME_LENGTH: 15,
+    MIN_PASSWORD_LENGTH: 5,
+    MAX_PASSWORD_LENGTH: 15,
+};
