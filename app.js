5cd08d2302db55cacf3712e1cece7e6b39bf7a80
Todor Dinkov <fasttakerbg@yahoo.com>
2017-07-04 17:40:20 +0300

    + log in , sign up pages and home button

--- app.js
+++ app.js
@@ -15,12 +15,15 @@ const mainRoutes = require('./src/routes');
 const allBikesRoutes = require('./src/routes/allBikes.js');
 const sparePartsRoutes = require('./src/routes/spareParts.js');
 const accessoriesRoutes = require('./src/routes/accessories.js');
-
+const loginRoutes = require('./src/routes/login.js');
+const signupRoutes = require('./src/routes/signup.js');
 
 app.use('/', mainRoutes);
 app.use('/allbikes', allBikesRoutes);
 app.use('/Bike-Spare-Parts', sparePartsRoutes);
 app.use('/accessories', accessoriesRoutes);
+app.use('/login', loginRoutes);
+app.use('/signup', signupRoutes);
 
 // app.get('/', (req, res) => { // using PUG
 //     res.send(homeController.loadHome);
@@ -37,15 +40,15 @@ app.use('/accessories', accessoriesRoutes);
 app.use((req, res, next) => {
     const err = new Error('Not Found. Please verify you have entered a valid address')
     err.status = 404;
-    next(err)
-})
+    next(err);
+});
 
 //error handler
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
     res.render('error');
-})
+});
 
 app.listen(constants.APP_PORT, () => {
     const date = new Date();
