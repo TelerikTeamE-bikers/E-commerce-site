a4e3db64d07a32cce1f81e8693c5a1a579c17b1b
Savata <sava.ivanov.9@gmail.com>
2017-07-05 21:26:33 +0300

    New application entry point logic

--- app.js
+++ app.js
@@ -1,54 +1,56 @@
-const express = require('express');
+// const express = require('express');
 
-// const homeController = require('./src/controllers/home-controller'); // Not sure how to use it yet
-//const bikeController = require('./src/controllers/bike-controller');
+// // const homeController = require('./src/controllers/home-controller'); // Not sure how to use it yet
+// //const bikeController = require('./src/controllers/bike-controller');
 
-const app = express();
-//app.use(express.static(path.join('temp')));
-app.use('/static', express.static('public'));
-app.set('view engine', 'pug');
-app.set('views', './src/views');
+// const app = express();
+// //app.use(express.static(path.join('temp')));
+// app.use('/static', express.static('public'));
+// app.set('view engine', 'pug');
+// app.set('views', './src/views');
 
 
-const constants = require('./src/common/constants');
-const mainRoutes = require('./src/routes');
-const allBikesRoutes = require('./src/routes/allBikes.js');
-const sparePartsRoutes = require('./src/routes/spareParts.js');
-const accessoriesRoutes = require('./src/routes/accessories.js');
-const loginRoutes = require('./src/routes/login.js');
-const signupRoutes = require('./src/routes/signup.js');
+// const constants = require('./src/common/constants');
+// const mainRoutes = require('./src/routes');
+// const allBikesRoutes = require('./src/routes/allBikes.js');
+// const sparePartsRoutes = require('./src/routes/spareParts.js');
+// const accessoriesRoutes = require('./src/routes/accessories.js');
+// const loginRoutes = require('./src/routes/login.js');
+// const signupRoutes = require('./src/routes/signup.js');
 
-app.use('/', mainRoutes);
-app.use('/allbikes', allBikesRoutes);
-app.use('/Bike-Spare-Parts', sparePartsRoutes);
-app.use('/accessories', accessoriesRoutes);
-app.use('/login', loginRoutes);
-app.use('/signup', signupRoutes);
+// app.use('/', mainRoutes);
+// app.use('/allbikes', allBikesRoutes);
+// app.use('/Bike-Spare-Parts', sparePartsRoutes);
+// app.use('/accessories', accessoriesRoutes);
+// app.use('/login', loginRoutes);
+// app.use('/signup', signupRoutes);
 
-// app.get('/', (req, res) => { // using PUG
-//     res.send(homeController.loadHome);
+// // app.get('/', (req, res) => { // using PUG
+// //     res.send(homeController.loadHome);
+// // });
+
+// /*app.get('/404', (req, res) => {
+//     res.status(404).send({
+//         error: 'Page not found.',
+//         name: 'Todo App v1.0',
+//     });
+// });*/
+
+// //custom error handler
+// app.use((req, res, next) => {
+//     const err = new Error('Not Found. Please verify you have entered a valid address')
+//     err.status = 404;
+//     next(err);
 // });
 
-/*app.get('/404', (req, res) => {
-    res.status(404).send({
-        error: 'Page not found.',
-        name: 'Todo App v1.0',
-    });
-});*/
+// //error handler
+// app.use((err, req, res, next) => {
+//     res.locals.error = err;
+//     res.status(err.status);
+//     res.render('error');
+// });
 
-//custom error handler
-app.use((req, res, next) => {
-    const err = new Error('Not Found. Please verify you have entered a valid address')
-    err.status = 404;
-    next(err);
-});
-
-//error handler
-app.use((err, req, res, next) => {
-    res.locals.error = err;
-    res.status(err.status);
-    res.render('error');
-});
+const app = require('./src/core/application');
 
 app.listen(constants.APP_PORT, () => {
     const date = new Date();
