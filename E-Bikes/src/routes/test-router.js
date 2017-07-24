15ea53eb8a9bc657d3e726cb83cfca02e270eb00
Savata <sava.ivanov.9@gmail.com>
2017-07-24 21:25:04 +0300

    removed some unused code

--- E-Bikes/src/routes/test-router.js
+++ E-Bikes/src/routes/test-router.js
@@ -4,8 +4,8 @@ module.exports = (app, controllers) => {
     let router = new express.Router();
     let controller = controllers.user;
 
-    router
-        .get('/testroute', controller.registerUser);
+    // router
+    //     .get('/testroute', controller.registerUser);
 
     app.use('/', router);
 
