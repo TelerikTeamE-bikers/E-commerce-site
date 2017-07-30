7e2366c92c05c60220c96ddf9090e0c5a96f144a
Savata <sava.ivanov.9@gmail.com>
2017-07-30 20:19:16 +0300

    added some base repository tests

--- E-Bikes/src/test/integration/home.test.js
+++ E-Bikes/src/test/integration/home.test.js
@@ -2,33 +2,33 @@ const { init } = require('../../core/engine');
 const request = require('supertest');
 const { expect } = require('chai');
 
-// next lines are from Doncho presentation - how to adopted for our project
-describe('/items tests', () => {
-    const connectionString = 'mongodb://localhost/items-db-test';
-    let app = null;
+// // next lines are from Doncho presentation - how to adopted for our project
+// describe('/items tests', () => {
+//     const connectionString = 'mongodb://localhost/items-db-test';
+//     let app = null;
 
-    beforeEach(() => {
-        return Promise.resolve()
-            .then(() => require('../../db').init(connectionString))
-            .then((db) => require('../../data').init(db))
-            .then((data) => require('../../app').init(data))
-            .then((_app) => {
-                app = _app;
-            });
-    });
+//     beforeEach(() => {
+//         return Promise.resolve()
+//             .then(() => require('../../db').init(connectionString))
+//             .then((db) => require('../../data').init(db))
+//             .then((data) => require('../../app').init(data))
+//             .then((_app) => {
+//                 app = _app;
+//             });
+//     });
 
-    describe('GET /item', () => {
-        it('expect to return 200', (done) => {
-            request(app)
-                .get('/items')
-                .expect(200)
-                .end((err, res) => {
-                    if (err) {
-                        return done(err);
-                    }
+//     describe('GET /item', () => {
+//         it('expect to return 200', (done) => {
+//             request(app)
+//                 .get('/items')
+//                 .expect(200)
+//                 .end((err, res) => {
+//                     if (err) {
+//                         return done(err);
+//                     }
 
-                    return done();
-                });
-        });
-    });
-});
+//                     return done();
+//                 });
+//         });
+//     });
+// });
